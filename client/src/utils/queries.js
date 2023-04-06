6942c9cc8a6a47dc2197cb8ab57d4baaa69220be
michelle <113802263+michelletrn@users.noreply.github.com>
2023-04-06 17:18:08 -0400

    queries and sample responses for reference added

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -49,3 +49,188 @@ query User($id: ID!) {
 //       }
 //     }
 //   }
+
+export const QUERY_ALLUSERS = gql`
+  query allUsers {
+    allUsers {
+      _id
+      username
+      firstname
+      lastname
+      projects {
+        projectName
+        _id
+      }
+    }
+  }
+`;
+
+// Response for allUsers query:
+// {
+//   "data": {
+//     "allUsers": [
+//       {
+//         "_id": "642dc577a1ed87faaf26d3e8",
+//         "username": "mtran",
+//         "firstname": "Michelle",
+//         "lastname": "Tran",
+//         "projects": [
+//           {
+//             "projectName": "project name",
+//             "_id": "642dc664a1ed87faaf26d3ed"
+//           }
+//         ]
+//       }
+//     ]
+//   }
+// }
+
+export const QUERY_PROJECTBYID = gql`
+  query projectbyid($id: ID) {
+    project(_id: $id) {
+      _id
+      tags {
+        name
+      }
+      projectRequirements
+      projectName
+      projectDescription
+      owner {
+        _id
+        username
+        firstname
+        lastname
+      }
+      collaborators {
+        username
+        _id
+        firstname
+        lastname
+      }
+    }
+  }
+`;
+
+// Response for ProjectByID query
+// {
+//   "data": {
+//     "project": {
+//       "_id": "642dc664a1ed87faaf26d3ed",
+//       "tags": [
+//         {
+//           "name": "tech"
+//         }
+//       ],
+//       "projectRequirements": "project req",
+//       "projectName": "project name",
+//       "projectDescription": "desc",
+//       "owner": {
+//         "_id": "642dc577a1ed87faaf26d3e8",
+//         "username": "mtran",
+//         "firstname": "Michelle",
+//         "lastname": "Tran"
+//       },
+//       "collaborators": []
+//     }
+//   }
+// }
+
+export const QUERY_ALLPROJECTS = gql`
+  query AllProjects {
+    allProjects {
+      _id
+      collaborators {
+        _id
+        username
+        firstname
+        lastname
+      }
+      owner {
+        _id
+        username
+      }
+      projectDescription
+      projectName
+      projectRequirements
+      tags {
+        name
+      }
+    }
+  }
+`;
+
+//response for all projects query:
+// {
+//   "data": {
+//     "allProjects": [
+//       {
+//         "_id": "642dc664a1ed87faaf26d3ed",
+//         "collaborators": [],
+//         "owner": {
+//           "_id": "642dc577a1ed87faaf26d3e8",
+//           "username": "mtran"
+//         },
+//         "projectDescription": "desc",
+//         "projectName": "project name",
+//         "projectRequirements": "project req",
+//         "tags": [
+//           {
+//             "name": "tech"
+//           }
+//         ]
+//       }
+//     ]
+//   }
+// }
+
+export const QUERY_PROJECTBYTAG = gql`
+  query ProjectByTag($name: String!) {
+    projectByTag(name: $name) {
+      tags {
+        name
+      }
+      _id
+      projectName
+      projectDescription
+      projectRequirements
+      owner {
+        _id
+        username
+        firstname
+        lastname
+      }
+      collaborators {
+        _id
+        username
+        firstname
+        lastname
+      }
+    }
+  }
+`;
+
+//response for project by tags
+// {
+//   "data": {
+//     "projectByTag": [
+//       {
+//         "tags": [
+//           {
+//             "name": "tech"
+//           }
+//         ],
+//         "_id": "642dc664a1ed87faaf26d3ed",
+//         "projectName": "project name",
+//         "projectDescription": "desc",
+//         "projectRequirements": "project req",
+//         "owner": {
+//           "_id": "642dc577a1ed87faaf26d3e8",
+//           "username": "mtran",
+//           "firstname": "Michelle",
+//           "lastname": "Tran"
+//         },
+//         "collaborators": []
+//       }
+//     ]
+//   }
+// }
