8415afc46a1bfa1a0f5b211b1fd91c1d550519ff
val.gee <vguevara0806@gmail.com>
2023-05-09 18:09:54 -0400

    finished log in page with styling

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/header/Header';
 import Footer from "./components/footer/Footer";
 import Home from './pages/home/Home';
-import Login from './pages/login/login';
+import Login from './pages/login/Login';
 import Signup from "./pages/signup/Signup";
 
 
--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -54,7 +54,7 @@ const Signup = () => {
         }}
       >
         {data ? (
-          <Link to="/">home</Link>
+          <Link to="/">Home</Link>
         ) : (
           <form>
             <div
